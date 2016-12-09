5962ca9d5d5228b51e8c51d8cd5590c139297a41
Aram Drevekenin <grimsniffer@gmail.com>
2016-12-09 17:12:43 +0100

    fix: remove wrapping flags

--- src/xterm.js
+++ src/xterm.js
@@ -1818,6 +1818,12 @@ function wrapLines (unwrappedLine, x, defAttr) {
   })
 }
 
+function removeWrappingFlags (line) {
+  return line
+    .filter(c => !c[3] || c[3] !== 'pad')
+    .map(c => [c[0], c[1], c[2]])
+}
+
 /**
  * Resizes the terminal.
  *
@@ -1851,9 +1857,9 @@ Terminal.prototype.resize = function(x, y) {
     // unwrap all lines
     var unwrappedLine = formLine(l, memo, index, memo.length - 1)
     if (unwrappedLine) {
-      memo[memo.length - 1] = unwrappedLine
+      memo[memo.length - 1] = removeWrappingFlags(unwrappedLine)
     } else {
-      memo.push(l)
+      memo.push(removeWrappingFlags(l))
     }
     return memo
   }, [])
